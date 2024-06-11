6d9074fac0b0f49fd04d65921c5d1e0b4ac2e90c
Marek Pridal <marek@marekpridal.eu>
2024-06-11 15:23:12 +0200

    Ad UTs

--- Tests/SwinjectLightTests/SwinjectLightTests.swift
+++ Tests/SwinjectLightTests/SwinjectLightTests.swift
@@ -1,12 +1,29 @@
 import XCTest
 @testable import SwinjectLight
 
-final class SwinjectLightTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+private protocol MockDependency: AnyObject { }
+private class MockClass: MockDependency { }
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+final class SwinjectLightTests: XCTestCase {
+    func testRegisterAndResolve() {
+        let container = Container()
+        let localVariable = MockClass()
+        container.register(MockDependency.self) { _ in
+            MockClass()
+        }
+        let resolvedDependency = container.resolve(MockDependency.self)
+        XCTAssertTrue(resolvedDependency is MockClass)
+        XCTAssertFalse(localVariable === resolvedDependency)
+    }
+
+    func testSharedInstace() {
+        let container = Container()
+        let localVariable = MockClass()
+        container.register(MockDependency.self) { _ in
+            localVariable
+        }
+        let resolvedDependency = container.resolve(MockDependency.self)
+        XCTAssertTrue(resolvedDependency is MockClass)
+        XCTAssertTrue(localVariable === resolvedDependency)
     }
 }

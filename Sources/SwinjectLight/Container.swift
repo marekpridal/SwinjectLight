5a32301814a8e9f89dfafa30c9a97334c2fe1906
Marek Pridal <marek@marekpridal.eu>
2024-06-11 15:04:14 +0200

    Revert "Use own queue to prevent data race"
    This reverts commit 19cf26425415e180573e70a8de8dde5af08b7444.

--- Sources/SwinjectLight/Container.swift
+++ Sources/SwinjectLight/Container.swift
@@ -5,31 +5,26 @@ public protocol Resolver: AnyObject {
 }
 
 public final class Container: @unchecked Sendable {
-    private let queue = DispatchQueue(label: "com.marekpridal.swinjectlight.container", qos: .userInteractive)
     private var storage: [String: Any] = [:]
 
     public init() { }
 
     public func register<Service>(_ type: Service.Type, factory: @escaping (Resolver) -> Service) {
         let key = "\(type)"
-        queue.sync {
-            storage[key] = factory
-        }
+        storage[key] = factory
     }
 }
 
 extension Container: Resolver {
     public func resolve<Service>(_ type: Service.Type) -> Service {
         let key = "\(type)"
-        return queue.sync {
-            guard let factory = storage[key] else {
-                fatalError("Dependency \(key) has not been registered")
-            }
-            guard let typedFactory = factory as? (((Resolver) -> Service)?) else {
-                fatalError("Dependency \(key) did not provide factory closure")
-            }
-            let service = typedFactory?(self)
-            return service!
+        guard let factory = storage[key] else {
+            fatalError("Dependency \(key) has not been registered")
         }
+        guard let typedFactory = factory as? (((Resolver) -> Service)?) else {
+            fatalError("Dependency \(key) did not provide factory closure")
+        }
+        let service = typedFactory?(self)
+        return service!
     }
 }

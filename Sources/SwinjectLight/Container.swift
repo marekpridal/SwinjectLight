19cf26425415e180573e70a8de8dde5af08b7444
Marek Pridal <marek@marekpridal.eu>
2024-06-11 15:02:07 +0200

    Use own queue to prevent data race

--- Sources/SwinjectLight/Container.swift
+++ Sources/SwinjectLight/Container.swift
@@ -5,26 +5,31 @@ public protocol Resolver: AnyObject {
 }
 
 public final class Container: @unchecked Sendable {
+    private let queue = DispatchQueue(label: "com.marekpridal.swinjectlight.container", qos: .userInteractive)
     private var storage: [String: Any] = [:]
 
     public init() { }
 
     public func register<Service>(_ type: Service.Type, factory: @escaping (Resolver) -> Service) {
         let key = "\(type)"
-        storage[key] = factory
+        queue.sync {
+            storage[key] = factory
+        }
     }
 }
 
 extension Container: Resolver {
     public func resolve<Service>(_ type: Service.Type) -> Service {
         let key = "\(type)"
-        guard let factory = storage[key] else {
-            fatalError("Dependency \(key) has not been registered")
+        return queue.sync {
+            guard let factory = storage[key] else {
+                fatalError("Dependency \(key) has not been registered")
+            }
+            guard let typedFactory = factory as? (((Resolver) -> Service)?) else {
+                fatalError("Dependency \(key) did not provide factory closure")
+            }
+            let service = typedFactory?(self)
+            return service!
         }
-        guard let typedFactory = factory as? (((Resolver) -> Service)?) else {
-            fatalError("Dependency \(key) did not provide factory closure")
-        }
-        let service = typedFactory?(self)
-        return service!
     }
 }
